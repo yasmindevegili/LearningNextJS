cc02c037599bcd5b50cbeec0f94dd9fdbd7934c6
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2023-06-12 16:14:30 -0300

    Crud inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Yasmin', 20, '2'),
+    new Cliente('Bia', 37, '3'),
+    new Cliente('Marcos', 34, '4'),
+  ]
+  return (
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-blue-500 to-purple-500
+    text-white`}>
+      <Layout titulo="Cadastro Simples" >
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
+    </div>
+  )
+}
